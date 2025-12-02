195c34d83659e1a01c152b9687f86593f1faf3be
4Mgr1 <4Mgr1@DESKTOP-EHD8S3P>
2025-12-02 11:52:30 +0100

    zadanie z komentarzem

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -1,6 +1,6 @@
 ﻿class Program
 {
-    static void Main() 
+    static void Main() //program działą poprawnie
     {
         int dzien = 15, miesiac = 11, rok = 2024;
         DateTime data = new DateTime(rok, miesiac, dzien);
